a0d2474ab9e53faeffa03896cefd1d1fa44b8f0f
Eric Lu <revenge.ez.123@gmail.com>
2025-07-08 13:07:36 -0400

    Changed titleBar color, and removed navbar widget

--- coolapp/lib/views/widget_tree.dart
+++ coolapp/lib/views/widget_tree.dart
@@ -2,32 +2,49 @@ import 'package:coolapp/views/pages/home_page.dart';
 import 'package:coolapp/views/pages/profile_page.dart';
 import 'package:coolapp/views/pages/videos_page.dart';
 import 'package:flutter/material.dart';
-import 'package:coolapp/widgets/navbar_widget.dart';
 
-List<Widget> pages = [HomePage(), ProfilePage(), VideosPage()]; //update pages
-
-class WidgetTree extends StatelessWidget {
+class WidgetTree extends StatefulWidget {
   const WidgetTree({super.key});
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  int _selectedIndex = 1; // Default to ProfilePage
+  static final List<Widget> _pages = <Widget>[
+    HomePage(),
+    ProfilePage(),
+    VideosPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _pages[_selectedIndex],
       appBar: AppBar(
-        title: const Text('Vera'),
         centerTitle: true,
-        backgroundColor: Colors.teal,
+        title: Text("Vera"),
+        backgroundColor: const Color.fromARGB(255, 15, 48, 40),
       ),
-      /*drawer: Drawer(
-        backgroundColor: const Color.fromARGB(255, 2, 78, 72),
-        child: Column(
-          children: [
-            const DrawerHeader(child: Text('Options')),
-            const ListTile(title: Text('Login')),
-          ],
-        ),
-      ),*/
-      body: pages.elementAt(1),
-      bottomNavigationBar: NavbarWidget(),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onItemTapped,
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+          NavigationDestination(
+            icon: Icon(Icons.ondemand_video),
+            label: 'Videos',
+          ),
+        ],
+      ),
     );
   }
 }
