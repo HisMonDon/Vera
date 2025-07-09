4262299df7aecd6df88ec80da30225e7d264d095
Eric Lu <revenge.ez.123@gmail.com>
2025-07-08 22:11:35 -0400

    updated and replaced text with better texts, worked on design, added app bar, changed the container to a scaffold

--- coolapp/lib/views/pages/locked_page.dart
+++ coolapp/lib/views/pages/locked_page.dart
@@ -1,33 +1,42 @@
 //page that displays whenever someone isn't subscribed
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LockedPage extends StatelessWidget {
   const LockedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color.fromARGB(255, 15, 48, 40),
-      child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Vera"),
+        backgroundColor: const Color.fromARGB(255, 15, 48, 40),
+      ),
+      backgroundColor: Color.fromARGB(255, 4, 34, 26),
+
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           AutoSizeText(
-            'Content Locked! Please get premium to access these videos.',
-            style: TextStyle(
-              fontSize: 50,
+            'Content Locked! Please purchase premium to access these videos.',
+            style: GoogleFonts.montserrat(
+              fontSize: 40,
               color: const Color.fromARGB(255, 255, 255, 255),
               decoration: TextDecoration.none,
             ), //starts at 50, will shrink to fit
             maxLines: 1,
           ),
-          Text(
+          AutoSizeText(
+            //change this when i get back from class
             'All revenue from subscriptions go towards charity!',
-            style: TextStyle(
+            style: GoogleFonts.openSans(
               fontSize: 30,
               color: Colors.white,
               decoration: TextDecoration.none,
             ),
+            maxLines: 1,
           ),
           SizedBox(height: 20),
           Icon(Icons.lock, size: 150),
