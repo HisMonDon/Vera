9127b80240058920bcf5be4fc67cae1b168dc5ab
Eric Lu <revenge.ez.123@gmail.com>
2025-07-09 13:57:41 -0400

    fixed minor firebase bug that prevented app startup

--- coolapp/lib/main.dart
+++ coolapp/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
