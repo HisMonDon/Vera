c98d026e64ed5c67031f32fcd8715f2337f94efa
Eric Lu <revenge.ez.123@gmail.com>
2025-07-09 16:35:01 -0400

    added login method successfully!

--- coolapp/lib/main.dart
+++ coolapp/lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:coolapp/firebase_options.dart';
 import 'package:coolapp/views/widget_tree.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // No Firebase initialization here
   runApp(MyApp());
 }
 
