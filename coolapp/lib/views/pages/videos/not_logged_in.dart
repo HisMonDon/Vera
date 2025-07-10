fed59095efdff2440fde7e0bbc28a7a73f3c4c75
Eric Lu <revenge.ez.123@gmail.com>
2025-07-10 15:00:54 -0400

    fixed centering problem

--- coolapp/lib/views/pages/videos/not_logged_in.dart
+++ coolapp/lib/views/pages/videos/not_logged_in.dart
@@ -19,43 +19,48 @@ class NotLoggedIn extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 15, 48, 40),
       ),
       backgroundColor: Color.fromARGB(255, 4, 34, 26),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          AutoSizeText(
-            'Content Locked!',
-            style: GoogleFonts.montserrat(
-              fontSize: 60,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              decoration: TextDecoration.none,
-            ), //starts at 60, will shrink to fit
-            maxLines: 1,
-          ),
-          AutoSizeText(
-            'Please create an account or log in to access these videos.',
-            style: GoogleFonts.montserrat(
-              fontSize: 30,
-              color: const Color.fromARGB(255, 255, 255, 255),
-              decoration: TextDecoration.none,
-            ), //starts at 30, will shrink to fit
-            maxLines: 1,
-          ),
-          SizedBox(height: 20),
-          Icon(Icons.lock, size: 150),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => ProfilePage()),
-              );
-            },
-            child: Text(
-              'Login or Sign Up',
-              style: GoogleFonts.montserrat(fontSize: 20, color: Colors.white),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AutoSizeText(
+              'Content Locked!',
+              style: GoogleFonts.montserrat(
+                fontSize: 60,
+                color: const Color.fromARGB(255, 255, 255, 255),
+                decoration: TextDecoration.none,
+              ), //starts at 60, will shrink to fit
+              maxLines: 1,
             ),
-          ),
-        ],
+            AutoSizeText(
+              'Please create an account or log in to access these videos.',
+              style: GoogleFonts.montserrat(
+                fontSize: 30,
+                color: const Color.fromARGB(255, 255, 255, 255),
+                decoration: TextDecoration.none,
+              ), //starts at 30, will shrink to fit
+              maxLines: 1,
+            ),
+            SizedBox(height: 20),
+            Icon(Icons.lock, size: 150),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProfilePage()),
+                );
+              },
+              child: Text(
+                'Login or Sign Up',
+                style: GoogleFonts.montserrat(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
