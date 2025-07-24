db4448b9438b409f97442a23ee19e6d6a9b0da07
Eric Lu <revenge.ez.123@gmail.com>
2025-07-24 10:18:36 -0400

    added widget mask, trying to get image to upload

--- coolapp/lib/views/pages/home/home_page.dart
+++ coolapp/lib/views/pages/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:coolapp/globals.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:ui' as ui;
 import 'dart:async';
+import 'package:widget_mask/widget_mask.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,50 +20,17 @@ class _HomePageState extends State<HomePage> {
           padding: EdgeInsets.all(20),
           child: Column(
             children: [
-              MaskedImage(
-                image: AssetImage('images/text_background.jpg'),
-                child: Text(
-                  welcomeText,
-                  style: GoogleFonts.mPlus1(
-                    fontSize: 30,
-                    color: Colors.transparent,
-                  ),
+              WidgetMask(
+                mask: Image(
+                  image: AssetImage('text_background.jpg'),
+                  fit: BoxFit.cover,
                 ),
+                child: Text(welcomeText),
               ),
             ],
           ),
         ),
       ),
     );
-  }
-}
-
-class MaskedImage extends StatelessWidget {
-  final ImageProvider image;
-  final Widget child;
-  const MaskedImage({required this.image, required this.child});
-  @override
-  Widget build(BuildContext context) => FutureBuilder<ui.Image>(
-    future: loadImage(),
-    builder: (context, snap) => snap.hasData
-        ? ShaderMask(
-            blendMode: BlendMode.srcATop,
-            shaderCallback: (bounds) => ImageShader(
-              snap.data!,
-              TileMode.clamp,
-              TileMode.clamp,
-              Matrix4.identity().storage,
-            ),
-            child: child,
-          )
-        : Center(child: CircularProgressIndicator()),
-  );
-  Future<ui.Image> loadImage() async {
-    final completer = Completer<ui.Image>();
-    final stream = image.resolve(ImageConfiguration());
-    stream.addListener(
-      ImageStreamListener((info, _) => completer.complete(info.image)),
-    );
-    return completer.future;
   }
 }
