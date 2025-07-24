f20c861ebad4a148944882c6d1b3715313bc9f77
Eric Lu <revenge.ez.123@gmail.com>
2025-07-24 10:45:03 -0400

    added image mask thing, going to change color and add name and time

--- coolapp/lib/views/pages/home/home_page.dart
+++ coolapp/lib/views/pages/home/home_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:coolapp/globals.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'dart:ui' as ui;
-import 'dart:async';
 import 'package:widget_mask/widget_mask.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +10,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String welcomeText = 'Hello!';
+  String welcomeText = 'Welcome To Vera';
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
@@ -21,11 +19,21 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             children: [
               WidgetMask(
+                blendMode: BlendMode.srcATop,
+                childSaveLayer: true,
                 mask: Image(
-                  image: AssetImage('text_background.jpg'),
+                  image: AssetImage('images/text_background.jpg'),
                   fit: BoxFit.cover,
                 ),
-                child: Text(welcomeText),
+                child: Text(
+                  welcomeText,
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 100,
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    decoration: TextDecoration.none,
+                  ),
+                ),
               ),
             ],
           ),
