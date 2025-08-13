0934a58d775416de0c27778c620e91aabfb3ac0e
Eric Lu <revenge.ez.123@gmail.com>
2025-08-13 21:01:10 +0800

    added sizedBox to control size

--- coolapp/lib/views/pages/home/home_page.dart
+++ coolapp/lib/views/pages/home/home_page.dart
@@ -555,9 +555,15 @@ class _HomePageState extends State<HomePage> {
                                       color: thumbnailColor,
                                     ),
                                   ),
-                                  Image(
-                                    image: AssetImage(
-                                      'images/featured_video_background.png',
+                                  SizedBox(
+                                    height: 500,
+                                    width:
+                                        (MediaQuery.of(context).size.width) -
+                                        120,
+                                    child: Image(
+                                      image: AssetImage(
+                                        'images/featured_video_background.png',
+                                      ),
                                     ),
                                   ),
                                   Column(
