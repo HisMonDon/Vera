e94a89563df3906bd5d17843da7d1ca8466de676
Eric Lu <revenge.ez.123@gmail.com>
2025-08-26 21:58:44 +0800

    added animation package, working on animations

--- coolapp/lib/views/pages/about_this_app/about_this_app_page.dart
+++ coolapp/lib/views/pages/about_this_app/about_this_app_page.dart
@@ -19,6 +19,7 @@ import 'dart:ui' as ui;
 //import 'package:media_kit_video/media_kit_video.dart';
 //import 'dart:io';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:animate_do/animate_do.dart';
 
 class AboutThisAppPage extends StatelessWidget {
   const AboutThisAppPage({super.key});
@@ -27,12 +28,38 @@ class AboutThisAppPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TimedAppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: SingleChildScrollView(child: Column()),
-        ),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          if (index.isEven) {
+            return FadeInLeft(
+              delay: Duration(milliseconds: 100 * index),
+              duration: Duration(milliseconds: 500),
+              child: buildListItem(index),
+            );
+          } else {
+            return FadeInRight(
+              delay: Duration(milliseconds: 100 * index),
+              duration: Duration(milliseconds: 500),
+              child: buildListItem(index),
+            );
+          }
+        },
       ),
     ); //rememrber appbar
+  }
+
+  Widget buildListItem(int index) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 4.0,
+        child: ListTile(
+          title: Text('box'),
+          subtitle: Text('text :D'),
+          contentPadding: EdgeInsets.all(16),
+        ),
+      ),
+    );
   }
 }
