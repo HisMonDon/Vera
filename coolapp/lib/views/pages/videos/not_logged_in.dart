7e6d8442d103a01b8d74e2b89fffe1e00fd67091
Eric Lu <revenge.ez.123@gmail.com>
2025-10-18 21:12:55 -0400

    added blur background to locked page

--- coolapp/lib/views/pages/videos/not_logged_in.dart
+++ coolapp/lib/views/pages/videos/not_logged_in.dart
@@ -13,79 +13,87 @@ class NotLoggedIn extends StatelessWidget {
     return Scaffold(
       appBar: TimedAppBar(),
       backgroundColor: Color.fromARGB(255, 4, 34, 26),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AutoSizeText(
-              'Content Locked!',
-              style: GoogleFonts.mPlus1(
-                fontSize: 80,
-                color: const Color.fromARGB(255, 255, 255, 255),
-                decoration: TextDecoration.none,
-              ),
-              maxLines: 1,
-            ),
-            WidgetMask(
-              blendMode: BlendMode.srcATop,
-              childSaveLayer: true,
-              mask: Image(
-                image: AssetImage('images/text_background.jpg'),
-                fit: BoxFit.cover,
-              ),
-              child: AutoSizeText(
-                'Please create a FREE account or log in to access UNLIMITED free videos.',
-                style: GoogleFonts.mPlus1(
-                  fontSize: 40,
-                  color: const Color.fromARGB(255, 255, 255, 255),
-                  decoration: TextDecoration.none,
-                ),
-                maxLines: 1,
-              ),
-            ),
-
-            SizedBox(height: 20),
-            Icon(Icons.lock, size: 150, color: Colors.white),
-            ElevatedButton(
-              onPressed: () {
-                //Update the selected index
-                global.selectedIndex = 1;
-
-                // Navigate to widget tree with profile tab selected
-                Navigator.of(
-                  //basaiclly clears the stack very useful especially for home page
-                  context,
-                  rootNavigator: true,
-                ).pushNamedAndRemoveUntil(
-                  '/widgetTree',
-                  (route) => false,
-                  arguments: 1,
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                backgroundColor: const Color.fromARGB(255, 28, 150, 109),
-                foregroundColor: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  SizedBox(height: 5),
-                  Text(
-                    'Login or Sign Up',
-                    style: GoogleFonts.montserrat(
-                      fontSize: 20,
-                      color: Colors.white,
-                    ),
+      body: Stack(
+        children: [
+          Image.asset(
+            'images/locked_screen_blurred.png',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                AutoSizeText(
+                  'Content Locked!',
+                  style: GoogleFonts.mPlus1(
+                    fontSize: 80,
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    decoration: TextDecoration.none,
                   ),
-                  SizedBox(height: 5),
-                ],
-              ),
+                  maxLines: 1,
+                ),
+                WidgetMask(
+                  blendMode: BlendMode.srcATop,
+                  childSaveLayer: true,
+                  mask: Image(
+                    image: AssetImage('images/text_background.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                  child: AutoSizeText(
+                    'Please create a FREE account or log in to access UNLIMITED free videos.',
+                    style: GoogleFonts.mPlus1(
+                      fontSize: 40,
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      decoration: TextDecoration.none,
+                    ),
+                    maxLines: 1,
+                  ),
+                ),
+
+                SizedBox(height: 20),
+                Icon(Icons.lock, size: 150, color: Colors.white),
+                ElevatedButton(
+                  onPressed: () {
+                    global.selectedIndex = 1;
+
+                    Navigator.of(
+                      //basaiclly clears the stack very useful especially for home page
+                      context,
+                      rootNavigator: true,
+                    ).pushNamedAndRemoveUntil(
+                      '/widgetTree',
+                      (route) => false,
+                      arguments: 1,
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    backgroundColor: const Color.fromARGB(255, 28, 150, 109),
+                    foregroundColor: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 5),
+                      Text(
+                        'Login or Sign Up',
+                        style: GoogleFonts.montserrat(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
