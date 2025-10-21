4be17c108de22c617fe39b841e0d135982b96d23
Eric Lu <revenge.ez.123@gmail.com>
2025-10-21 17:25:25 -0400

    removed redundant pubspec extract

--- coolapp/windows/flutter/generated_plugin_registrant.cc
+++ coolapp/windows/flutter/generated_plugin_registrant.cc
@@ -8,7 +8,6 @@
 
 #include <media_kit_libs_windows_video/media_kit_libs_windows_video_plugin_c_api.h>
 #include <media_kit_video/media_kit_video_plugin_c_api.h>
-#include <thumblr_windows/thumblr_windows_plugin.h>
 #include <volume_controller/volume_controller_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
@@ -16,8 +15,6 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("MediaKitLibsWindowsVideoPluginCApi"));
   MediaKitVideoPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("MediaKitVideoPluginCApi"));
-  ThumblrWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ThumblrWindowsPlugin"));
   VolumeControllerPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("VolumeControllerPluginCApi"));
 }
