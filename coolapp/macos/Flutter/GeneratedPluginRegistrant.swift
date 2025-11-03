ead035e3f851d8cf2bd7d10cd2e184cf26fe5882
Eric Lu <revenge.ez.123@gmail.com>
2025-11-03 08:46:14 -0500

    fixed github deploy

--- coolapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ coolapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import media_kit_video
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
+import thumblr_macos
 import volume_controller
 import wakelock_plus
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  ThumblrMacosPlugin.register(with: registry.registrar(forPlugin: "ThumblrMacosPlugin"))
   VolumeControllerPlugin.register(with: registry.registrar(forPlugin: "VolumeControllerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
