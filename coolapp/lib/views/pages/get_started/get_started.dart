c42aafeff5655fe932aad3fa3efced41e0b819fe
Eric Lu <revenge.ez.123@gmail.com>
2025-12-24 10:15:17 -0500

    finalized autoscroll

--- coolapp/lib/views/pages/get_started/get_started.dart
+++ coolapp/lib/views/pages/get_started/get_started.dart
@@ -213,7 +213,7 @@ class _AboutThisAppPageState extends State<AboutThisAppPage> {
   }
 
   void _startAutoScroll() {
-    _timer = Timer.periodic(const Duration(seconds: 3), (timer) {
+    _timer = Timer.periodic(const Duration(milliseconds: 1), (timer) {
       if (!mounted) {
         timer.cancel();
         return;
@@ -223,19 +223,11 @@ class _AboutThisAppPageState extends State<AboutThisAppPage> {
           _sampleImagesController.position.maxScrollExtent;
       const double itemWidth = 600.0;
       const double horizontalPadding = 8.0;
-      const double scrollAmount = itemWidth + (horizontalPadding * 2);
+      const double scrollAmount = itemWidth + (horizontalPadding * 7);
       if (currentOffset >= maxScrollExtent) {
-        _sampleImagesController.animateTo(
-          0,
-          duration: const Duration(milliseconds: 500),
-          curve: Curves.easeInOut,
-        );
+        _sampleImagesController.jumpTo(0);
       } else {
-        _sampleImagesController.animateTo(
-          currentOffset + scrollAmount,
-          duration: const Duration(milliseconds: 500),
-          curve: Curves.easeInOut,
-        );
+        _sampleImagesController.jumpTo(currentOffset + scrollAmount);
       }
     });
   }
@@ -249,35 +241,31 @@ class _AboutThisAppPageState extends State<AboutThisAppPage> {
     'images_tutorial/display6.png',
     'images_tutorial/display7.png',
   ];
+
   Widget _buildSampleImagesRow() {
-    return Scrollbar(
+    return SingleChildScrollView(
       controller: _sampleImagesController,
-      thumbVisibility: true,
-      interactive: true,
-      child: SingleChildScrollView(
-        controller: _sampleImagesController,
-        scrollDirection: Axis.horizontal,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(displayPaths.length, (index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Container(
-                  width: 600,
-                  height: 300,
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 18, 59, 49),
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(color: Colors.grey),
-                      image: DecorationImage(
-                          image: AssetImage(displayPaths[index]),
-                          fit: BoxFit.cover)),
-                ),
-              );
-            }),
-          ),
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(displayPaths.length, (index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Container(
+                width: 600,
+                height: 300,
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 18, 59, 49),
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.grey),
+                    image: DecorationImage(
+                        image: AssetImage(displayPaths[index]),
+                        fit: BoxFit.cover)),
+              ),
+            );
+          }),
         ),
       ),
     );
